118ea546465f5bcdb3711bc0a2ae76f2ddc7ca82
David Silwal <david.silwal@ekbana.net>
2018-01-12 09:06:59 +0545

    no changes yet

--- dotnetnepal/Infrastructure/ApplicationDbContext.cs
+++ dotnetnepal/Infrastructure/ApplicationDbContext.cs
@@ -2,9 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Infrastructure.Models.Interfaces;
--- dotnetnepal/Infrastructure/DatabaseInitializer.cs
+++ dotnetnepal/Infrastructure/DatabaseInitializer.cs
@@ -1,12 +1,8 @@
 ﻿
 using Infrastructure.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Infrastructure.Core;
 using Infrastructure.Core.Interfaces;
